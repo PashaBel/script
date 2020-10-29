e489e2f9c747bb0d8ac7ea068ef79298dd7a81ab
PashaBel <pashabel@gmail.com>
2020-10-29 15:13:28 +0300

    -//-

--- archiv.py
+++ archiv.py
@@ -6,12 +6,14 @@ from datetime import date
 today = date.today()
 pathForArchiv = 'D:\\zip'
 pathArchiv = 'D:\\zipout\\'
+
 zip_arhiv = zipfile.ZipFile(pathArchiv + str(today.year) + '_' + str(today.month) + '.zip', 'w')
 
 for folder, subfolder, files in os.walk(pathForArchiv):   
     for file in files:
         if file.endswith('.CSV'):
             zip_arhiv.write(os.path.join(folder, file), file, compress_type = zipfile.ZIP_DEFLATED)
+
 zip_arhiv.close()
 
 fileInZip = zipfile.ZipFile(pathArchiv + str(today.year) + '_' + str(today.month) + '.zip', 'r')
